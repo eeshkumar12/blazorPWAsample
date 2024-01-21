cd1173d01fa4bfcac00654ecbae41bf5ee9609e1
Eeshkumar Siddenthi <eeshkumar.s@Technovert.com>
2024-01-21 11:14:51 +0530

    added data repository

---
+++ deps/DateLibrary/DataRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateLibrary
+{
+    public class DataRepository
+    {
+        public string[] names = { "name1", "name2" };
+        public string[] GetNames()
+        {
+            return names;
+        }
+    }
+
+}

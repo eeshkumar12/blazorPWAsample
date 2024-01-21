33dfc3daa396842dc3d1b20d7f8b9b735c942a1c
Eeshkumar Siddenthi <eeshkumar.s@Technovert.com>
2024-01-21 12:01:25 +0530

    added name9

--- deps/DateLibrary/DataRepository.cs
+++ deps/DateLibrary/DataRepository.cs
@@ -8,7 +8,7 @@ namespace DateLibrary
 {
     public class DataRepository
     {
-        public string[] names = { "name1", "name2", "name3", "name4", "name5", "name6","name7","name8" };
+        public string[] names = { "name1", "name2", "name3", "name4", "name5", "name6","name7","name8","name9" };
         public string[] GetNames()
         {
             return names;
